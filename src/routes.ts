31a304eb38cc78cec70b1c5778bd6c001d4922d8
Charles Lana <charleslanop@gmail.com>
2020-12-19 20:26:14 -0300

    Update server

--- src/routes.ts
+++ src/routes.ts
@@ -22,19 +22,26 @@ routes.post('/locations',
     upload.single('image'),
     celebrate({
         body: Joi.object().keys({
-            name: Joi.string().required(),
+            name: Joi.string().required().messages({
+                'string.base': `"name" deve ser um tipo de 'texto'`,
+                'string.empty': `"name" não pode ser um campo vazio`,
+                'string.min': `"name" deve ter um tamanho mínimo de {#limit}`,
+                'string.max': `"name" deve ter um tamanho máximo de {#limit}`,
+                'any.required': `"name" é um campo obrigatório`
+            }),
             email: Joi.string().required().email(),
-            whatsapp: Joi.number().required().max(15),
+            whatsapp: Joi.number().required().min(9),
             latitude: Joi.number().required(),
             longitude: Joi.number().required(),
             city: Joi.string().required(),
             uf: Joi.string().required().max(2),
-            items: Joi.array().required(),
+            items: Joi.string().required().min(1),
         })
     }, {
         abortEarly: false
     }),
     locationsController.create
+
 );
 
 export default routes;
