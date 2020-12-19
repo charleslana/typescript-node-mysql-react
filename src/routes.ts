414c12cec60e79cfe04162acf56cf19c5aa0e8d8
Charles Lana <charleslanop@gmail.com>
2020-12-19 15:25:17 -0300

    Update Route

--- src/routes.ts
+++ src/routes.ts
@@ -24,7 +24,7 @@ routes.post('/locations',
         body: Joi.object().keys({
             name: Joi.string().required(),
             email: Joi.string().required().email(),
-            whatsapp: Joi.number().required(),
+            whatsapp: Joi.number().required().max(15),
             latitude: Joi.number().required(),
             longitude: Joi.number().required(),
             city: Joi.string().required(),
